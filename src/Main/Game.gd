a6001cc1e4f531bd51731f69ebca69bb7e29bd23
Léo Boyer <contact@leoboyer.fr>
2022-01-29 17:13:42 +0100

    Added pause menu ui

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -4,13 +4,31 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var _mainMenu = $Interface/MainMenu
+onready var _pauseMenu = $Interface/PauseMenu
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func restartCurrentLevel():
+	$Level.get_tree().reload_current_scene()
 
+func _unhandled_input(event):
+	# The GlobalControls node, in the Stage scene, is set to process even
+	# when the game is paused, so this code keeps running.
+	# To see that, select GlobalControls, and scroll down to the Pause category
+	# in the inspector.
+	if event.is_action_pressed("toggle_pause"):
+		var tree = get_tree()
+		tree.paused = not tree.paused
+		if tree.paused:
+			_pauseMenu.open()
+		else:
+			_pauseMenu.close()
+		get_tree().set_input_as_handled()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_PauseMenu_restart_level():
+	restartCurrentLevel()
---
+++ src/Menu/PauseMenu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+signal restart_level()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+
+func open():
+	$ColorRect/ResumeButton.grab_focus()
+	get_tree().paused = true;
+	show();
+
+func close():
+	get_tree().paused = false;
+	hide();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ResumeButton_pressed():
+	close()
+
+
+func _on_RestartButton_pressed():
+	close()
+	emit_signal('restart_level')
