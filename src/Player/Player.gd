b66d4d3eb0396c3fc5f47c16bc8dab7bed2efcb6
Léo Boyer <contact@leoboyer.fr>
2022-01-29 20:21:38 +0100

    add player sprites

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -4,11 +4,16 @@ export (int) var run_speed = 100
 export (int) var jump_speed = -400
 export (int) var gravity = 1200
 
+onready var sprite = $AnimatedSprite
+
 enum {IDLE, RUN, JUMP}
 var velocity = Vector2()
 var state: int
 var anim: String
 var new_anim: String
+
+func _ready():
+	sprite.play('idle')
 
 func change_state(new_state):
 	state = new_state
@@ -19,6 +24,7 @@ func change_state(new_state):
 			new_anim = 'run'
 		JUMP:
 			new_anim = 'jump'
+	sprite.play(new_anim)
 
 func get_input():
 	velocity.x = 0
@@ -26,20 +32,27 @@ func get_input():
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 
+	var isJump = state == JUMP
+
 	if jump and is_on_floor():
 		change_state(JUMP)
 		velocity.y += jump_speed
-	if right:
-		change_state(RUN)
+	elif right:
 		velocity.x += run_speed
-	if left:
-		change_state(RUN)
+		if not isJump: change_state(RUN)
+	elif left:
 		velocity.x -= run_speed
-	$Sprite.flip_h = velocity.x < 0
+		if not isJump: change_state(RUN)
+	elif not isJump: change_state(IDLE)
+	
+	if not sprite.flip_h and velocity.x < 0:
+		sprite.flip_h = true
+	if sprite.flip_h and velocity.x > 0:
+		sprite.flip_h = false
 
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
-	if state == JUMP and is_on_floor():
+	if state == JUMP and is_on_floor() and velocity.y > 0:
 			change_state(IDLE)
 	velocity = move_and_slide(velocity, Vector2(0, -1))
