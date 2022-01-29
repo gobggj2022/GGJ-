260af857a7c07e24c19c20901a71e959911cc811
Léo Boyer <contact@leoboyer.fr>
2022-01-29 12:34:46 +0100

    base tests

---
+++ src/GameCamera.gd
@@ -0,0 +1,15 @@
+class_name GameCamera
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hey")
+	pass # Replace with function body.
+
+
